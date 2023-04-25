098e3f10048e140837b3ea1596f11aa3700b9394
Mathias Gumpen Gundersen <Mathias.g.gundersen@live.no>
2023-04-25 19:23:40 +0200

    Easd

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 import 'flowbite';
 const Navbar = ({ language, setLanguage }) => {
 	const chooseLanguage = (lang) => {
@@ -33,7 +34,7 @@ const Navbar = ({ language, setLanguage }) => {
 						id='language-dropdown-menu'>
 						<ul class='py-2 font-medium' role='none'>
 							<li>
-								<a
+								<Link
 									onClick={() => chooseLanguage('norsk')}
 									href='#'
 									class='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white'
@@ -43,10 +44,10 @@ const Navbar = ({ language, setLanguage }) => {
 											? 'Norsk'
 											: 'Norwegian'}
 									</div>
-								</a>
+								</Link>
 							</li>
 							<li>
-								<a
+								<Link
 									onClick={() => chooseLanguage('english')}
 									href='#'
 									class='block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-600 dark:hover:text-white'
@@ -56,7 +57,7 @@ const Navbar = ({ language, setLanguage }) => {
 											? 'Engelsk (US)'
 											: 'English (US)'}
 									</div>
-								</a>
+								</Link>
 							</li>
 						</ul>
 					</div>
@@ -85,32 +86,32 @@ const Navbar = ({ language, setLanguage }) => {
 					id='mobile-menu-language-select'>
 					<ul class='flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700'>
 						<li>
-							<a
+							<Link
 								href='/'
 								class='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500'
 								aria-current='page'>
 								{language === 'norsk'
 									? '100 Spørsmål'
 									: '100 Questions'}
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								href='/add-questions'
 								class='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
 								{language === 'norsk'
 									? 'Legg til spørsmål'
 									: 'Add question'}
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
+							<Link
 								href='/about-us'
 								class='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700'>
 								{language === 'norsk'
 									? 'Om 100 Spørsmål'
 									: 'About 100 Questions'}
-							</a>
+							</Link>
 						</li>
 					</ul>
 				</div>
