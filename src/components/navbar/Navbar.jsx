7d824830aa4cfd7fb08b64da50682547eba3ace7
Mathias Gumpen Gundersen <Mathias.g.gundersen@live.no>
2023-04-25 19:26:05 +0200

    Easd

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import 'flowbite';
 const Navbar = ({ language, setLanguage }) => {
 	const chooseLanguage = (lang) => {
