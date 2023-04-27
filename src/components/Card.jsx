00bc3c8f743025563d9ce47b4082eefd11411608
Mathias Gumpen Gundersen <Mathias.g.gundersen@live.no>
2023-04-27 13:19:13 +0200

    asd

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,22 +23,8 @@ const Card = ({
 		return (
 			<AnimatePresence>
 				<motion.div
-					className={`z-20 shadow-md absolute border border-blue-700 font-semibold text-2xl w-[90%] h-full  text-white bg-blue-600 rounded-lg 
-				flex items-center justify-center`}>
-					<div className='absolute top-6 left-6 px-6 py-2 text-base rounded-full bg-blue-600 flex items-center justify-center'>
-						{'Question: ' + questionNumber + '.'}
-					</div>
-					<button
-						onClick={() => !first && paginate(1)}
-						className='h-full -translate-x-5 w-[50vw] md:w-40  absolute left-0  rounded'></button>
-					<div className='w-[90%] flex flex-col items-center justify-center'>
-						<div className='text-base'>Hvem i rommet</div>
-						<div className=''>{text}</div>
-					</div>
-					<button
-						onClick={() => !last && paginate(-1)}
-						className='h-full translate-x-5 w-[50vw] md:w-40  absolute right-0  rounded'></button>
-				</motion.div>
+					className={`z-20 relative shadow-lg  bg-blue-100  font-semibold text-2xl w-[95%] h-full  text-white  rounded-lg 
+				flex items-center justify-center`}></motion.div>
 			</AnimatePresence>
 		);
 	} else {
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,96 +1,143 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Navbar from "@/components/navbar/Navbar";
-import { useEffect, useState } from "react";
-import Card from "@/components/Card";
-import { AnimatePresence } from "framer-motion";
-import Hero from "@/components/hero/Hero";
-import Link from "next/link";
-
-import { AiFillQuestionCircle, AiOutlinePlusSquare } from "react-icons/ai";
-import { BsQuestionSquare } from "react-icons/bs";
-const inter = Inter({ subsets: ["latin"] });
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import Navbar from '@/components/navbar/Navbar';
+import { useEffect, useState } from 'react';
+import Card from '@/components/Card';
+import { AnimatePresence, spring } from 'framer-motion';
+import Hero from '@/components/hero/Hero';
+import Link from 'next/link';
+import { motion } from 'framer-motion';
+import { AiFillQuestionCircle, AiOutlinePlusSquare } from 'react-icons/ai';
+import { BsQuestionSquare } from 'react-icons/bs';
+const inter = Inter({ subsets: ['latin'] });
 
 const questions = [
-  { text: "Er alltid på mobilen?", first: true },
-  { text: "Har mest viljestyrke?" },
-  { text: "Er best på å styrte?" },
-  { text: "Er dårligst sjåfør?" },
-  { text: "Er mest vill?" },
-  { text: "Spanderer alltid på fylla?" },
-  { text: "Er kåtest?" },
-  { text: "Er flinkest på skolen?" },
-  { text: "Tar seg mest på puppene?" },
-  { text: "Trodde lengst på julenissen?" },
-  { text: "10 Spørsmål, SKÅL!", milestone: true },
-  { text: "Mest ubesluttsom?" },
-  { text: "Har høyest toleranse?" },
-  { text: "Er mest rettferdig?" },
-  { text: "Lager de sjukeste lydene i senga?" },
-  { text: "Gir mest til veldedighet?" },
-  { text: "Er mest overlegen?" },
-  { text: "Kommer til å lykkes best i livet?" },
-  { text: "Er en adrenalin junkie?" },
-  { text: "Googler seg selv oftest?" },
-  { text: "Er lykkeligst?" },
-  { text: "SKÅL!", milestone: true, last: true },
+	{ text: 'Er alltid på mobilen?', first: true },
+	{ text: 'Har mest viljestyrke?' },
+	{ text: 'Er best på å styrte?' },
+	{ text: 'Er dårligst sjåfør?' },
+	{ text: 'Er mest vill?' },
+	{ text: 'Spanderer alltid på fylla?' },
+	{ text: 'Er kåtest?' },
+	{ text: 'Er flinkest på skolen?' },
+	{ text: 'Tar seg mest på puppene?' },
+	{ text: 'Trodde lengst på julenissen?' },
+	{ text: '10 Spørsmål, SKÅL!', milestone: true },
+	{ text: 'Mest ubesluttsom?' },
+	{ text: 'Har høyest toleranse?' },
+	{ text: 'Er mest rettferdig?' },
+	{ text: 'Lager de sjukeste lydene i senga?' },
+	{ text: 'Gir mest til veldedighet?' },
+	{ text: 'Er mest overlegen?' },
+	{ text: 'Kommer til å lykkes best i livet?' },
+	{ text: 'Er en adrenalin junkie?' },
+	{ text: 'Googler seg selv oftest?' },
+	{ text: 'Er lykkeligst?' },
+	{ text: 'SKÅL!', milestone: true, last: true },
 ];
 
 export default function Home() {
-  const [language, setLanguage] = useState("norsk");
-  const [i, setIndex] = useState(0);
+	const [language, setLanguage] = useState('norsk');
+	const [i, setIndex] = useState(0);
 
-  return (
-    <>
-      <nav>
-        <Navbar language={language} setLanguage={setLanguage}></Navbar>
-      </nav>
-      <main
-        className={`overflow-x-hidden  flex flex-col items-center gap-12 h-auto  min-h-screen w-screen bg-white  ${inter.className}`}
-      >
-        <section>
-          <Hero></Hero>
-        </section>
-        <section className="  py-6 flex flex-col items-center justify-center w-screen h-auto min-h-92 gap-6">
-          <Link className="w-auto" href="sporsmal">
-            <button
-              type="button"
-              class="text-white  min-w-[300px] bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-8 py-4 text-center mr-2 mb-2"
-            >
-              Spill 100 Spørsmål
-            </button>
-          </Link>
-          <Link className="w-auto" href="add-questions">
-            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-              <span class="relative min-w-[300px] px-8 py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-                Lag dine egne spørsmål
-              </span>
-            </button>
-          </Link>
-          <div className="flex flex-col gap-6 my-10 items-center justify-center w-[90%]">
-            <div className=" h-36 bg-blue-100 w-full rounded-md flex items-center justify-evenly">
-              <div className="h-[60%] aspect-square  rounded flex items-center justify-center">
-                <BsQuestionSquare className="h-full w-full text-blue-300"></BsQuestionSquare>
-              </div>
-              <div className="h-[80%] w-[55%] px-4 flex flex-col items-start justify-center">
-                <h2 className="font-semibold text-lg text-blue-900">
-                  Ferdig lagde spørsmål
-                </h2>
-              </div>
-            </div>
-            <div className=" h-36 bg-blue-100 w-full rounded-md flex flex-row-reverse items-center justify-evenly">
-              <div className="h-[70%] aspect-square  rounded flex items-center justify-center">
-                <AiOutlinePlusSquare className=" h-full w-full text-blue-300"></AiOutlinePlusSquare>
-              </div>
-              <div className="h-[70%] w-[55%] px-4 flex flex-col items-start justify-center">
-                <h2 className="font-semibold text-lg text-blue-900">
-                  Lag dine egne spørsmål
-                </h2>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-    </>
-  );
+	return (
+		<>
+			<nav>
+				<Navbar language={language} setLanguage={setLanguage}></Navbar>
+			</nav>
+			<main
+				className={`overflow-x-hidden  flex flex-col items-center gap-12 h-auto  min-h-screen w-screen bg-white  ${inter.className}`}>
+				<section>
+					<Hero></Hero>
+				</section>
+				<section className='  py-6 flex flex-col items-center justify-center w-screen h-auto min-h-92 gap-6'>
+					<Link className='w-auto' href='sporsmal'>
+						<button
+							type='button'
+							class='text-white  min-w-[300px] bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-8 py-4 text-center mr-2 mb-2'>
+							Spill 100 Spørsmål
+						</button>
+					</Link>
+					<Link className='w-auto' href='add-questions'>
+						<button class='relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800'>
+							<span class='relative min-w-[300px] px-8 py-4 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0'>
+								Lag dine egne spørsmål
+							</span>
+						</button>
+					</Link>
+					<div className='flex flex-col  gap-6 my-10 items-center justify-center w-[90%]'>
+						<motion.div
+							initial={{ x: 200 }}
+							whileInView={{ x: 0 }}
+							transition={{
+								duration: 0.1,
+								type: 'spring',
+								bounce: 0.2,
+								mass: 1,
+								stiffness: 70,
+							}}
+							viewport={{
+								once: true,
+							}}
+							className=' h-32 shadow bg-blue-100 w-full rounded-md flex items-center justify-evenly'>
+							<div className='h-[60%] aspect-square  rounded flex items-center justify-center'>
+								<BsQuestionSquare className='h-full w-full text-blue-300'></BsQuestionSquare>
+							</div>
+							<div className='h-[80%] w-[55%] px-4 flex flex-col items-start justify-center'>
+								<h2 className='font-semibold text-lg text-blue-900'>
+									Ferdig lagde spørsmål
+								</h2>
+							</div>
+						</motion.div>
+						<motion.div
+							initial={{ x: 200 }}
+							whileInView={{ x: 0 }}
+							transition={{
+								duration: 0.1,
+								type: 'spring',
+								bounce: 0.2,
+								mass: 1,
+								stiffness: 70,
+							}}
+							viewport={{
+								once: true,
+							}}
+							className=' h-32 shadow bg-blue-100 w-full rounded-md flex flex-row-reverse items-center justify-evenly'>
+							<div className='h-[70%] aspect-square  rounded flex items-center justify-center'>
+								<AiOutlinePlusSquare className=' h-full w-full text-blue-300'></AiOutlinePlusSquare>
+							</div>
+							<div className='h-[70%] w-[55%] px-4 flex flex-col items-start justify-center'>
+								<h2 className='font-semibold text-lg text-blue-900'>
+									Lag dine egne spørsmål
+								</h2>
+							</div>
+						</motion.div>
+						<motion.div
+							initial={{ x: 200 }}
+							whileInView={{ x: 0 }}
+							transition={{
+								duration: 0.1,
+								type: 'spring',
+								bounce: 0.2,
+								mass: 1,
+								stiffness: 70,
+							}}
+							viewport={{
+								once: true,
+							}}
+							className=' h-32 shadow bg-blue-100 w-full rounded-md flex items-center justify-evenly'>
+							<div className='h-[60%] aspect-square  rounded flex items-center justify-center'>
+								<BsQuestionSquare className='h-full w-full text-blue-300'></BsQuestionSquare>
+							</div>
+							<div className='h-[80%] w-[55%] px-4 flex flex-col items-start justify-center'>
+								<h2 className='font-semibold text-lg text-blue-900'>
+									Del spørsmål
+								</h2>
+							</div>
+						</motion.div>
+					</div>
+				</section>
+			</main>
+		</>
+	);
 }
